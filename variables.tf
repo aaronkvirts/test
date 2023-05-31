2adb8efc6d223882aef884917ced38b2644d9863
VS-AaronKho <110884337+aaronkvirts@users.noreply.github.com>
2023-05-31 12:13:01 +0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "ALICLOUD_ACCESS_KEY" {
+  type        = string
+  description = "Access key"
+  default     = ""
+}
+
+variable "ALICLOUD_SECRET_KEY" {
+  type        = string
+  description = "Secret key"
+  default     = ""
+}
